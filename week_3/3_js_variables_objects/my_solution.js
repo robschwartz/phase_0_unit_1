96682260fd78ac4ac6a10993feff15a81075d7af
robschwartz <robertschwar@gmail.com>
2014-05-27 19:00:07 -0500

    updated 3.3

--- week_3/3_js_variables_objects/my_solution.js
+++ week_3/3_js_variables_objects/my_solution.js
@@ -1,4 +1,4 @@
-// I paired [by myself, with:] on this challenge.
+// I paired with Sebatian Radloff on this challenge.
 
 
 
@@ -12,32 +12,6 @@
 
 // __________________________________________
 // Write your code below.
-
-
-
-
-
-
-
-// __________________________________________
-// Refactored Code: Include a refactored version (or justification of your original code) here. 
-
-
-
-
-
-// __________________________________________
-// Reflection: Use the reflection guidelines to write a reflection here. 
-// 
-// 
-// 
-// 
-// 
-// 
-
-
-// __________________________________________
-// Driver Test Code:  Do not alter code below this line.
 
 function assert(test, message, test_number) {
   if (!test) {
@@ -113,6 +87,95 @@ assert(
 )
 
 members.push("Mary");
+
+assert(
+  members[3] === "Mary",
+  "The fourth element in the value of members should be 'Mary'.",
+  "9. "
+)
+
+
+
+
+
+// __________________________________________
+// Refactored Code: Include a refactored version (or justification of your original code) here. 
+
+// There wasn't too much to refactor here... 
+
+
+
+// __________________________________________
+// Reflection: Use the reflection guidelines to write a reflection here. 
+// 
+// It was really awesome pairing with someone on this one. 
+// He helped me get through the challange and was able to explain what we had to do really well. 
+// He drove most of the time while I navigated. 
+// 
+// 
+// 
+// 
+
+
+// __________________________________________
+// Driver Test Code:  Do not alter code below this line.
+
+function assert(test, message, test_number) {
+  if (!test) {
+    console.log(test_number + "false");
+    throw "ERROR: " + message;
+  }
+  console.log(test_number + "true");
+  return true;
+}
+
+assert(
+  (typeof secretNumber === 'number'),
+  "The value of secretNumber should be a number.",
+  "1. "
+)
+
+assert(
+  secretNumber === 7,
+  "The value of secretNumber should be 7.",
+  "2. "
+)
+
+assert(
+  typeof password === 'string',
+  "The value of password should be a string.",
+  "3. "
+)
+
+assert(
+  password === "just open the door",
+  "The value of password should be 'just open the door'.",
+  "4. "
+)
+
+assert(
+  typeof allowedIn === 'boolean',
+  "The value of allowedIn should be a boolean.",
+  "5. "
+)
+
+assert(
+  allowedIn === false,
+  "The value of allowedIn should be false.",
+  "6. "
+)
+
+assert(
+  members instanceof Array,
+  "The value of members should be an array",
+  "7. "
+)
+
+assert(
+  members[0] === "John",
+  "The first element in the value of members should be 'John'.",
+  "8. "
+)
 
 assert(
   members[3] === "Mary",
