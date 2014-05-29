0a4fa9eb25cb2aedf78e9a5e4892b057f46ebca6
robschwartz <robertschwar@gmail.com>
2014-05-28 19:25:19 -0500

    committing Pseudocode

---
+++ week_3/6_group_project/gc1_pseudocode.js
@@ -0,0 +1,20 @@
+/*our assumption is we are given the numbers and don't need to create a new array*/
+
+1) I want to be able to sum all the numbers in a group.
+#pseudocode
+create variable called total which is equal to 0
+loop through the number array (which was given to us) 
+pass the number array to a block which will add all the numbers
+
+2) I want to be able to calculate the mean of a group of numbers.
+#pseudocode
+Create new variable named mean 
+Have it equal to total / length of the array 
+
+3) I want to be able to calculate the median of a group of numbers.
+#pseudocode 
+First put the array in order 
+then find the length of the array
+then divide by 2 
+if even, subtract 1 and use that number to call that location in the array.
+if there's a remainder, round down and use that number to call the location in the array. 
