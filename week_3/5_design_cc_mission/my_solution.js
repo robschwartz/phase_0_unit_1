553857dc69376808c664d6a344cc607c0658a88e
robschwartz <robertschwar@gmail.com>
2014-05-29 07:44:42 -0500

    committing 3.5

--- week_3/5_design_cc_mission/my_solution.js
+++ week_3/5_design_cc_mission/my_solution.js
@@ -1,19 +1,61 @@
 // I worked [by myself, with:] on this challenge
 
 // Your mission description:
-// 
-// 
-// 
+// Title : The Run Around
+//  The level will be shaped sort of like this; -[]- 
+// // Start in a hallway where the ogres can't see me.
+// I will be on the left side and the ogres will be in the right corridor. The ogres are guarding a cell
+// of one of my warriors. The point of the level is to lure the ogres away from the cell, andhave the ogres 
+// chase the hero until they get tired.
+// Once they are tired, the hero will break down the door and run to freedom with the escapee.
+
 
 // Pseudocode
-// 
+// here are the steps the hero should take:
+walk right 
+walk down
+walk right
+walk up
+call to the ogres
+walk up
+walk left
+walk down
+walk down
+walk right
+walk up 
+walk right
+break down door
+tell escapee to follow him
+walk left
+walk up
+walk left
+walk down
+walk left
 // 
 // 
 // 
 // 
 
 // Initial Code
-
+this.moveRight();
+this.moveDown();
+this.moveRight();
+this.moveUp();
+this.say("Hey uglies!");
+this.moveUp();
+this.moveLeft();
+this.moveDown();
+this.moveDown();
+this.moveRight();
+this.moveUp();
+this.moveRight();
+this.attack();
+this.say("Follow me");
+this.moveLeft();
+this.moveUp();
+this.moveLeft();
+this.moveDown();
+this.moveLeft();
 
 
 
@@ -27,7 +69,8 @@
 
 
 // Reflection
-// 
+// This challange was pretty simple. I am assuming we do these challeges 
+// to practice the syntax of the language more than actually creating a level.
 // 
 // 
 // 
